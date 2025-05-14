14005722b3fafd1b61ac414d6edc7ddc670edb5e
Peetii <merxspace@outlook.com>
2025-05-14 11:45:56 -0300

    Adicionando modificações no html e css do portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,37 @@ import style from './App.module.css'
 function App() {
   return (
     <>
-      <h1 className={style.title}>Olá mundo</h1>
-    </>
+<nav className={style.menu}>
+  <p><a href="">Apresentação</a></p>
+  <p><a href="">Tecnologias</a></p>
+  <p><a href="">Projetos</a></p>
+  <p><a href="">Contato</a></p>
+  </nav>   
+
+  <main>
+    <section className ={style.s1}>
+      primeira
+    </section>
+    
+    <section className ={style.s2}>
+      segunda
+    </section>
+
+    <section className ={style.s3}>
+      terceira
+    </section>
+
+    <section className ={style.s4}>
+      quarto
+    </section>
+
+  </main>
+
+  <footer>
+
+    </footer> 
+    <h3>Redes Sociais</h3>
+  </>
   )
 }
 
